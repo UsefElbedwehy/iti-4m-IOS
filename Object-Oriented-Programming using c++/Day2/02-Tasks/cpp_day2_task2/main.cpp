99239cf90c2ae9e391346525a1ac37876911f8a0
UsefElbedwehy <elbedwehyy@gmail.com>
2024-12-03 19:07:55 +0200

    add display function as a (1- member function 2-friend 3-standalone )

--- Object-Oriented-Programming using c++/Day2/02-Tasks/cpp_day2_task2/main.cpp
+++ Object-Oriented-Programming using c++/Day2/02-Tasks/cpp_day2_task2/main.cpp
@@ -13,10 +13,25 @@ private:
     int top;
     int * data;
     static int stackCounter;
+    int stackID;
+    friend void DisplayStack2(Stack s);
 public:
     Stack(int inStackSize):stackSize(inStackSize),top(-1){
         data = new int[stackSize];
         stackCounter++;
+        stackID = stackCounter;
+    }
+    int sizeGetter(){
+        return stackSize;
+    }
+    int topGetter(){
+        return top;
+    }
+    int idGetter(){
+        return stackID;
+    }
+    int * dataGetter(){
+        return data;
     }
     int isEmpty(){
         if(top == EMPTY_STACK_TOP_VALUE){
@@ -51,15 +66,23 @@ public:
     }
     void DisplayStack(){
         int i=top;
-        cout << "Stack: [ ";
-        while(i > -1){
-            if((i-1) != -1){
-                cout << data[i] << ", ";
-            }else{
-            cout << data[i] << " ]" << endl;
+        if(top == -1){
+            cout << "Stack" << stackID << " is empty..." <<endl;
+        }else{
+            cout << "Stack"<< stackID <<": [ ";
+            while(i > -1){
+                if((i-1) != -1){
+                    cout << data[i] << ", ";
+                }else{
+                cout << data[i] << " ]" << endl;
+                }
+                i--;
             }
-            i--;
         }
+    }
+
+    void displayStackNumber(){
+    cout << "Number of stacks are: " << stackCounter << endl;
     }
 
     ~Stack(){
@@ -68,10 +91,45 @@ public:
 };
 int Stack::stackCounter=0;
 
+void DisplayStack2(Stack s){
+        int i= s.top;
+        if(s.top == -1){
+            cout << "Stack" << s.stackID << " is empty..." <<endl;
+        }else{
+            cout << "Stack"<< s.stackID <<": [ ";
+            while(i > -1){
+                if((i-1) != -1){
+                    cout << s.data[i] << ", ";
+                }else{
+                cout << s.data[i] << " ]" << endl;
+                }
+                i--;
+            }
+        }
+}
+void DisplayStack3(Stack s){
+    int top = s.topGetter();
+    int i = top;
+        if(top == -1){
+            cout << "Stack" << s.idGetter() << " is empty..." <<endl;
+        }else{
+            cout << "Stack"<< s.idGetter() <<": [ ";
+            while(i > -1){
+                if((i-1) != -1){
+                    cout << s.dataGetter()[i] << ", ";
+                }else{
+                cout << s.dataGetter()[i] << " ]" << endl;
+                }
+                i--;
+            }
+        }
+}
 
 int main()
 {
     Stack S1(5);
+    Stack S2(5);
+    Stack S3(5);
     S1.push(10);
     S1.push(12);
     S1.push(14);
@@ -81,7 +139,15 @@ int main()
     S1.pop();
     S1.pop();
     S1.push(200);
+    S1.pop();
+    S2.push(33);
+    S2.push(334);
+    S2.push(666);
     S1.DisplayStack();
+    DisplayStack2(S2);
+    DisplayStack3(S3);
+    S1.displayStackNumber();
+
 
     return 0;
 }
