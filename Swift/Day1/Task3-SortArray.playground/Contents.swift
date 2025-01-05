1d539b8f3aeb326aea40043110c689e1adfa057a
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-05 15:00:48 +0200

    write a function called: sortArray that takes an array

--- Swift/Day1/Task3-SortArray.playground/Contents.swift
+++ Swift/Day1/Task3-SortArray.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var arr:[Int] = [ 4 , 2 , 5 , 8 , 5 , 3 , 9 ,1 ,0 ]
+print("Bsfore sort: \(arr)")
+
+func sortArray(srcArr: inout [Int]) {
+    for j in 0..<srcArr.count-1 {
+        for i in 0..<srcArr.count-1-j {
+        if srcArr[i] > srcArr[i+1] {
+            let temp = srcArr[i]
+            srcArr[i] = srcArr[i+1]
+            srcArr[i+1] = temp
+        }
+      }
+    }
+}
+
+sortArray(srcArr: &arr)
+print("After  sort: \(arr)")
+
+
