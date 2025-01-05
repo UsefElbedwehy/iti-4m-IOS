631556492bcbba3ee16d861f1764768c13acff15
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-05 15:00:02 +0200

    Write a function called: clacPower that takes two numbers base and power and returns the base to that power as an Int

--- Swift/Day1/Task2-POWER.playground/Contents.swift
+++ Swift/Day1/Task2-POWER.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func calcPower(base: Int, power: Int ) -> Int {
+    var res = 1
+    for index in 1...power{
+        res = res * base
+    }
+    return res
+}
+var b = 10
+var p = 3
+print("result of power \(p) of base \(b) is equal : \(calcPower(base: b, power: p))")
