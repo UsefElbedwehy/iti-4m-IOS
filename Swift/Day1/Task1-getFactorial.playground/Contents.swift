00236426815c2f35996ab770bbdc35740c9df309
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-05 16:38:56 +0200

    add getFatorial2 to get factorial by recursion

--- Swift/Day1/Task1-getFactorial.playground/Contents.swift
+++ Swift/Day1/Task1-getFactorial.playground/Contents.swift
@@ -2,10 +2,21 @@ import UIKit
 var userNum = 5
 func getFactorial(number: Int) -> (Int) {
     var num = 1
-    for index in 2...number{
+    if number == 0{
+        return 0
+    }
+    for index in 1...number{
         num = num * index
     }
     return num
 }
-
 print("Factorial number of \(userNum) is \(getFactorial(number: userNum))")
+
+func getFactorialRec(number: Int) -> (Int) {
+    if number == 1{
+        return 1
+    }
+    return number * getFactorialRec(number: number-1)
+}
+
+print("Factorial number of \(userNum) is \(getFactorialRec(number: userNum))")
