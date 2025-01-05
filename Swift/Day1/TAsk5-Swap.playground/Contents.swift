f02d6c0e08556568bc502491f1905dd6783b53c9
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-05 15:02:06 +0200

    write a function called: swap that takes two numbers and swap them using inout parameters.

--- Swift/Day1/TAsk5-Swap.playground/Contents.swift
+++ Swift/Day1/TAsk5-Swap.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var firstNum = 11
+var secondNum = 44
+
+print("first number: \(firstNum) and second number : \(secondNum)")
+
+func swap(fNum: inout Int,sNum: inout Int) {
+    let temp = fNum
+    fNum = sNum
+    sNum = temp
+}
+
+swap(fNum: &firstNum, sNum: &secondNum)
+
+print("first number: \(firstNum) and second number : \(secondNum)")
