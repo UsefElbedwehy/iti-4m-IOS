25bc5a1c99625d2ccbbb2614e84b60f8de0ba16b
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-05 15:01:12 +0200

    write a function called: calMinAndMax that takes an array and returns the min and max numbers in it.

--- Swift/Day1/Task4-MinMax.playground/Contents.swift
+++ Swift/Day1/Task4-MinMax.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let arr:[Int] = [ 5 , 3 , 7 , 6 , 4 , 1 , 9 , 0 , 2]
+
+func calMinAndMax(srcArr:[Int]) -> (Int , Int) {
+    var maxNum = srcArr[0]
+    var minNum = srcArr[0]
+    for index in srcArr {
+        if index < minNum {
+            minNum = index
+        }else if index > maxNum{
+            maxNum = index
+        }
+    }
+    return (maxNum , minNum)
+}
+
+print("(max , min) : \(calMinAndMax(srcArr: arr))")
