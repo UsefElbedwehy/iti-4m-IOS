9704f66a74d16dc30f7822bb5cf9eb1a04c69ff7
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-15 20:28:33 +0200

    lec demo - get data from api

---
+++ Swift/Day6/lecDemo1/lecDemo1/News.swift
@@ -0,0 +1,17 @@
+//
+//  News.swift
+//  lecDemo1
+//
+//  Created by Usef on 15/01/2025.
+//
+
+import UIKit
+
+class News : Codable {
+    var author : String?
+    var title : String?
+    var desription : String?
+    var imageUrl : String?
+    var url : String?
+    var publishedAt : String?
+}
--- Swift/Day6/lecDemo1/lecDemo1/ViewController.swift
+++ Swift/Day6/lecDemo1/lecDemo1/ViewController.swift
@@ -16,26 +16,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loadNewsData(_ sender: Any) {
-        //1- URL
-        let url = URL(string: "")
-        //2-req
+        let indector = UIActivityIndicatorView(style: .large)
+        indector.center = view.center
+        indector.startAnimating()
+        self.view.addSubview(indector)
+        //1 - url
+        let url = URL(string: "https://raw.githubusercontent.com/DevTides/NewsApi/master/news.json")
+        //2 - request
         let request = URLRequest(url: url!)
-        //3-session
-        let session = URLSession(configuration: .default)
-        //4-task
-        let task = session.dataTask(with: request) { Data, request, error in
-//            do{
-//                
-//            }catch let error {
-//                
-//            }
+        
+        //3 - session
+        let session = URLSession(configuration:.default)
+        //4 - task
+        let task = session.dataTask(with: request){ data , response , error in
+            do{
+                /*let json = try JSONSerialization.jsonObject(with: data!) as! [[String:String]]
+                
+                let dict1 = json[0]
+                print("title: \(dict1["title"] ?? "...")")*/
+                
+                let json = try JSONDecoder().decode([News].self, from: data!)
+                
+                let news = json[0]
+               
+                DispatchQueue.main.async{
+                    self.newLB.text = news.title ?? "..."
+                    indector.stopAnimating()
+                }
+            }
+            catch let error
+            {
+                print(error.localizedDescription)
+            }
+            
         }
         
-        //5-resume task
         task.resume()
-        
-        
     }
-    
 }
 
