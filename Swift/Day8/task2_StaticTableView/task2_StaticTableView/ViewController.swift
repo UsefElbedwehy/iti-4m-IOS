d35e1c4160735eb2bdf0b9d1cdedb2003aac4b9d
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-18 15:50:35 +0200

    static table view

---
+++ Swift/Day8/task2_StaticTableView/task2_StaticTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task2_StaticTableView
+//
+//  Created by Usef on 18/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
