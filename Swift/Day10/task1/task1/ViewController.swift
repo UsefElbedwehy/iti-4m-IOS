f2e896533c9bcc30c7896223081850292f9e059d
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-20 23:01:37 +0200

    localization

---
+++ Swift/Day10/task1/task1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by Usef on 20/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var labelLB: UILabel!
+    
+    @IBOutlet weak var secondLB: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        labelLB.text = "Anghami"
+        secondLB.text = "Spotify"
+    }
+
+
+}
+
