f7c97182e2fd750310017dd5ee3a57257bd200c2
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-22 16:21:40 +0200

    book demo (self study)

---
+++ Swift/Book-Demo/TableViewDemo/EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,14 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Usef on 18/01/2025.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name: String
+    var decription: String
+    var usage: String
+}
